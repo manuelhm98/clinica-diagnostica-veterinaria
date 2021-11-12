e7c22bc47f7162f7eb02f02da0b09a3403e50b3c
tiburonchan3 <tiburonchan3@gmail.com>
2021-11-12 11:21:33 -0600

    todo ok

--- src/components/Quotes/Result.jsx
+++ src/components/Quotes/Result.jsx
@@ -3,6 +3,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { readResultsByQuote } from "../../redux/actions/result";
 import { formatRelative, subDays } from "date-fns";
 import { es } from "date-fns/locale";
+import { Link } from "react-router-dom";
 
 export default function Result({ quote }) {
   const dispatch = useDispatch();
@@ -11,27 +12,29 @@ export default function Result({ quote }) {
   useEffect(() => {
     return dispatch(readResultsByQuote(quote?.id));
   }, [quote, dispatch]);
-  console.log(result);
+  console.log(quote);
 
   return (
     <div>
       <p>
-        Fecha:{" "}
-        <span className="text-sm font-semibold mt-4">
-          {formatRelative(subDays(new Date(result?.date), 0), new Date(), {
-            locale: es,
-          })}
-        </span>
+        Paciente:{" "}
+        <span className="text-sm font-semibold">{quote?.patients?.names}</span>
       </p>
-      <p>Diagnostico: {" "}
-        <span className="text-sm font-semibold">{result.diagnosis}</span>
+      <p>
+        Diagnostico:{" "}
+        <span className="text-sm font-semibold">{result?.diagnosis}</span>
       </p>
-      <p>Sintomatologia: {" "}
+      <p>
+        Sintomatologia:{" "}
         <span className="text-sm font-semibold">{result?.symptomatology}</span>
       </p>
-      <p>Tratamiento: {" "}
+      <p>
+        Tratamiento:{" "}
         <span className="text-sm font-semibold">{result?.treatment}</span>
       </p>
+      <button className="bg-blue-600 text-white w-full mt-3 px-8 ml-4 float-right text-xs py-1 rounded">
+        <Link to="/add-quote">Agregar consulta</Link>
+      </button>
     </div>
   );
 }
