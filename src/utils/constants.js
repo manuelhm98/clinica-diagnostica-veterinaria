26c07c277b0837e47154ce6d604eb780874ca59d
danniel-contreras <contrerasdannniel@gmail.com>
2022-08-01 15:35:02 -0600

    edit constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,10 @@
+
+export const API_HOST = "http://159.65.219.24:8000/api"
+
 //export const API_HOST = "http://localhost:8000/api"
-//export const API_HOST = "http://137.184.41.16:8000/api"
 
-export const API_HOST = "http://localhost:8000/api"
+export const SOCKET_URL = "http://159.65.219.24:8000"
 
-//export const SOCKET_URL = "http://137.184.41.16:8000"
-
-export const SOCKET_URL = "http://localhost:8000"
+//export const SOCKET_URL = "http://localhost:8000"
 
 
